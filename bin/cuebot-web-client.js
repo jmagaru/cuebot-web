cf6e28ad10e28f907044fdb63b84e58cfbf6e8d4
jmagaru <jonathanl.magaru@pccw.com>
2020-09-03 15:24:46 +0800

    Added cucumber wrapper : Completed

--- bin/cuebot-web-client.js
+++ bin/cuebot-web-client.js
@@ -9,13 +9,27 @@ const path = require("path");
 const fs = require("fs");
 const colors = require("chalk");
 
-module.exports.default = () => {
-  console.log(`[TEST] -  Executing Cucumber client JS`);
+module.exports.default = ({ specs, tags }) => {
+  // Recreating screenshot folders
+  if (!fs.existsSync("reports/snapshots")) {
+    fs.mkdirSync("reports/snapshots", { recursive: true });
+  }
+
+  // Catenate Client options
+  let clientOptions = ["", ""];
+  if (specs) clientOptions = clientOptions.concat([specs]);
+  if (tags) clientOptions = clientOptions.concat(["--tags", `${tags}`]);
+
+  let client = new (require("cucumber").Cli)({
+    argv: clientOptions,
+    cwd: process.cwd(),
+    stdout: process.stdout,
+  });
 
   // Returning Exit Code
   return new Promise(function (resolve, reject) {
     try {
-      return cli
+      return client
         .run()
         .then((result) => resolve(result.success === true ? 0 : 1));
     } catch (e) {
